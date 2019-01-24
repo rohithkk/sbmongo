d63d1a506589d8cd1fbbc2f16db9bf450e422710
rohithkk <rohithkodakandla@gmail.com>
2019-01-23 21:43:20 -0500

    project setup

---
+++ src/main/java/com/kodakandla/sbmongo/SbmongoApplication.java
@@ -0,0 +1,14 @@
+package com.kodakandla.sbmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbmongoApplication.class, args);
+	}
+
+}
+
